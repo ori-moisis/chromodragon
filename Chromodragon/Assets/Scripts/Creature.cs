b482ccc58c7a040fd33f2e2075f595b408995b04
Hagai <vafels@gmail.com>
2015-07-30 23:19:32 +0300

    Created a simple creature prefab! (woo)

---
+++ Chromodragon/Assets/Scripts/Creature.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creature : MonoBehaviour {
+
+    public bool hasRed = false;
+    public bool hasYellow = false;
+    public bool hasBlue = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
