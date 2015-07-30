76741df8fc0066395f5a64ea63b963b18c860e92
Barak Yoresh <barakyoresh@gmail.com>
2015-07-31 00:23:21 +0300

    added slingshot basic mechanics

---
+++ Chromodragon/Assets/Scripts/ShotBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehaviour : MonoBehaviour {
+	Rigidbody rb;
+	public float gravityAddition;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	void FixedUpdate() {
+		//make shots dsapear if their off screen for too long
+		if (transform.position.magnitude > 200) {
+			print ("i want to die");
+			Destroy(gameObject);
+		}
+
+		//make shots drop faster
+		rb.AddForce (-Vector3.up * gravityAddition);
+	}
+}
