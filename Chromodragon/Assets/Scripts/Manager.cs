7051c6ac2a8295b17adb9e73cf22705baccf8154
Hagai <vafels@gmail.com>
2015-07-31 11:23:03 +0300

    neighbor calculation

--- Chromodragon/Assets/Scripts/Manager.cs
+++ Chromodragon/Assets/Scripts/Manager.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Manager : MonoBehaviour {
 
     public GameObject CreaturePrefab;
     public GameObject hexTilePrefab;
 
+    // Map hexagon-cube coordinates to creatures:
+    GameObject[, ,] coordToCreature;
+
+    // Map creaturee to hexagon-cube coordinates:
+    Dictionary<GameObject, int[]> creatureToCoord = new Dictionary<GameObject, int[]>();
+
+	// Use this for initialization
+	void Start () {
+	    initWorld(3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+
+    GameObject[] getNeighbours(GameObject creature)
+    {
+        GameObject[] neighbours = new GameObject[6];
+        int[] coord = creatureToCoord[creature];
+
+        neighbours[0] = coordToCreature[coord[0], coord[1], coord[2]];
+        neighbours[0] = coordToCreature[coord[0], coord[1], coord[2]];
+        neighbours[0] = coordToCreature[coord[0], coord[1], coord[2]];
+
+        return neighbours;
+    }
+
+
     // Initiates the creatures in the world according to the specified radius (number of creatures in each axis excluding the middle one)
     void initWorld(int gridRadius)
     {
-        Creature[, ,] hexGrid = new Creature[gridRadius, gridRadius, gridRadius];
+        coordToCreature = new GameObject[gridRadius + 1, gridRadius + 1, gridRadius + 1];
+
+        // Create creatures and tiles:
         for (int x = 0; x <= gridRadius; ++x)
         {
             for (int y = 0; y <= gridRadius; ++y)
@@ -26,6 +59,11 @@ public class Manager : MonoBehaviour {
                         GameObject newCreature = Instantiate(CreaturePrefab);
                         newCreature.transform.position = new Vector3(newX, 0.25f, newZ);
 
+                        // add creature to data structures
+                        coordToCreature[x, y, z] = newCreature;
+                        int[] coord = new int[3] { x, y, z };
+                        creatureToCoord.Add(newCreature, coord);
+
                         // Create a new tile:
                         GameObject newTile = Instantiate(hexTilePrefab);
                         newTile.transform.position = new Vector3(newX, 0.01f, newZ);
@@ -34,14 +72,4 @@ public class Manager : MonoBehaviour {
             }
         }
     }
-
-	// Use this for initialization
-	void Start () {
-	    initWorld(3);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
