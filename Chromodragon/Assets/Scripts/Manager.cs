e31ec525d9baff661b983676745fef3a9a431263
Hagai <vafels@gmail.com>
2015-07-31 12:19:47 +0300

    turned manager to singleton added getneighbors function tiles and creatures are now created under an object

--- Chromodragon/Assets/Scripts/Manager.cs
+++ Chromodragon/Assets/Scripts/Manager.cs
@@ -4,8 +4,14 @@ using System.Collections.Generic;
 
 public class Manager : MonoBehaviour {
 
+    public static Manager instance;
+
     public GameObject CreaturePrefab;
     public GameObject hexTilePrefab;
+    public GameObject creatures;
+    public GameObject tiles;
+
+    public int hexRadius = 3;
 
     // Map hexagon-cube coordinates to creatures:
     GameObject[, ,] coordToCreature;
@@ -14,8 +20,12 @@ public class Manager : MonoBehaviour {
     Dictionary<GameObject, int[]> creatureToCoord = new Dictionary<GameObject, int[]>();
 
 	// Use this for initialization
-	void Start () {
-	    initWorld(3);
+	void Awake () {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        initWorld(hexRadius);
 	}
 	
 	// Update is called once per frame
@@ -24,14 +34,37 @@ public class Manager : MonoBehaviour {
 	}
 
 
-    GameObject[] getNeighbours(GameObject creature)
+    public GameObject[] getNeighbours(GameObject creature)
     {
-        GameObject[] neighbours = new GameObject[6];
+        GameObject[] neighbours = new GameObject[6]{null, null, null, null, null, null};
         int[] coord = creatureToCoord[creature];
 
-        neighbours[0] = coordToCreature[coord[0], coord[1], coord[2]];
-        neighbours[0] = coordToCreature[coord[0], coord[1], coord[2]];
-        neighbours[0] = coordToCreature[coord[0], coord[1], coord[2]];
+        if (coord[0] > 0)
+        {
+            neighbours[0] = coordToCreature[coord[0]-1, coord[1], coord[2]];
+        }
+        if (coord[0] < hexRadius)
+        {
+            neighbours[1] = coordToCreature[coord[0] + 1, coord[1], coord[2]];
+        }
+
+        if (coord[1] > 0)
+        {
+            neighbours[2] = coordToCreature[coord[0], coord[1] - 1, coord[2]];
+        }
+        if (coord[1] < hexRadius)
+        {
+            neighbours[3] = coordToCreature[coord[0], coord[1] + 1, coord[2]];
+        }
+
+        if (coord[2] > 0)
+        {
+            neighbours[4] = coordToCreature[coord[0], coord[1], coord[2] - 1];
+        }
+        if (coord[2] < hexRadius)
+        {
+            neighbours[5] = coordToCreature[coord[0], coord[1], coord[2] + 1];
+        }
 
         return neighbours;
     }
@@ -57,6 +90,7 @@ public class Manager : MonoBehaviour {
 
                         // Create a new creature:
                         GameObject newCreature = Instantiate(CreaturePrefab);
+                        newCreature.transform.parent = creatures.transform;
                         newCreature.transform.position = new Vector3(newX, 0.25f, newZ);
 
                         // add creature to data structures
@@ -66,6 +100,7 @@ public class Manager : MonoBehaviour {
 
                         // Create a new tile:
                         GameObject newTile = Instantiate(hexTilePrefab);
+                        newTile.transform.parent = tiles.transform;
                         newTile.transform.position = new Vector3(newX, 0.01f, newZ);
                     }
                 }
