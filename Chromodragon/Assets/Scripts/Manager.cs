a663164344809cdedfc01c6958d0e353a46467eb
Hagai <vafels@gmail.com>
2015-07-31 10:24:39 +0300

    Added hexagon tiles

--- Chromodragon/Assets/Scripts/Manager.cs
+++ Chromodragon/Assets/Scripts/Manager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Manager : MonoBehaviour {
 
     public GameObject CreaturePrefab;
+    public GameObject hexTilePrefab;
 
     // Initiates the creatures in the world according to the specified radius (number of creatures in each axis excluding the middle one)
     void initWorld(int gridRadius)
@@ -17,10 +18,17 @@ public class Manager : MonoBehaviour {
                 {
                     if (x == 0 || y == 0 || z == 0)
                     {
-                        GameObject newCreature = Instantiate(CreaturePrefab);
-                        float newX = x - Mathf.Cos(Mathf.PI/3)*( y + z);
+                        // Calculate world coordinates from cube-hexagon coordinates:
+                        float newX = x - Mathf.Cos(Mathf.PI / 3) * (y + z);
                         float newZ = Mathf.Sin(Mathf.PI / 3) * (y - z);
+
+                        // Create a new creature:
+                        GameObject newCreature = Instantiate(CreaturePrefab);
                         newCreature.transform.position = new Vector3(newX, 0.25f, newZ);
+
+                        // Create a new tile:
+                        GameObject newTile = Instantiate(hexTilePrefab);
+                        newTile.transform.position = new Vector3(newX, 0.01f, newZ);
                     }
                 }
             }
